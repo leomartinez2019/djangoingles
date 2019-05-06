27cdbf03010e53b28f1914f720acc2538bf7d06d
leomartinez2019 <lowenhard@yahoo.com>
2019-05-06 12:57:38 +0000

    agregar opcion repetir quiz

--- practice/views.py
+++ practice/views.py
@@ -1,7 +1,8 @@
 import json
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Quiz, Pregunta, Opcion
 
